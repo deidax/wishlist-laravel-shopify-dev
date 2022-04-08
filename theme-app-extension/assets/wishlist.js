42a196d8822c47f5e0147381d7480e0667d6aa79
Sigma247 <khalildida93@gmail.com>
2022-04-08 14:08:21 +0100

    updating wishlist.js to use axios

--- theme-app-extension/assets/wishlist.js
+++ theme-app-extension/assets/wishlist.js
@@ -1,26 +1,26 @@
-// nofy lib
-var script = document.createElement('script');
-script.type = 'text/javascript';
+// add nofy cdn
+javascriptCdn('https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.js')
+cssCdn('https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.css');
 
-script.src = 'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.js';
-document.body.appendChild(script);
-
-var link = document.createElement('link');
-link.rel = 'stylesheet';
-
-link.href = 'https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.css';
-document.body.appendChild(link);
+// axios cdn
+javascriptCdn('https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.js');
 
 
 // wishlist function
 
 function myFunction() {
   const button = document.querySelector('.wishlist-button')
+  // Get product id
+  const product_id = button.dataset.product
+  // Get customer id
+  const customer_id = button.dataset.customer
+  // Add product to wishlist
   if(button.classList.contains('active')){
     button.classList.remove('active')
     button.innerText = "Remove From Wishlist";
-    notification('success', 'Product added to wishlist')
+    notification('success', "Product ("+product_id+") added to wishlist")
   }
+  // remove product from wishlist
   else{
     button.classList.add('active')
     button.innerText = "Add To Wishlist";
@@ -28,6 +28,7 @@ function myFunction() {
   }
 }
 
+// notify user
 function notification(type, text){
   new Noty({
     type: type,
@@ -35,4 +36,23 @@ function notification(type, text){
     text: text,
     timeout: 3000
 }).show();
+}
+
+// add javascript cdn
+
+function javascriptCdn(cdn){
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  
+  script.src = cdn;
+  document.body.appendChild(script);
+}
+
+// add css cdn
+function cssCdn(cdn){
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  
+  link.href = cdn;
+  document.body.appendChild(link);
 }
