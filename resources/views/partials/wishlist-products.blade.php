56dc09b5429bec7cff066ac543bacc97f1f1b9ef
Sigma247 <khalildida93@gmail.com>
2022-04-12 01:24:29 +0000

    new view for wishlist

--- resources/views/partials/wishlist-products.blade.php
+++ resources/views/partials/wishlist-products.blade.php
@@ -2,34 +2,44 @@
 
 
 <!-- component -->
-<div class="py-6">
-    <div class="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
-      <div class="w-1/3 bg-cover" style="background-image: url('https://images.unsplash.com/photo-1494726161322-5360d4d0eeae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80')">
-      </div> 
-      <div class="w-2/3 p-4">
-        <h1 class="text-gray-900 font-bold text-2xl">Backpack</h1>
-        <p class="mt-2 text-gray-600 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit In odit exercitationem fuga id nam quia</p>
-        <div class="flex item-center mt-2">
-          <svg class="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
-            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-          </svg>
-          <svg class="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
-            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-          </svg>
-          <svg class="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
-            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-          </svg>
-          <svg class="w-5 h-5 fill-current text-gray-500" viewBox="0 0 24 24">
-            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-          </svg>
-          <svg class="w-5 h-5 fill-current text-gray-500" viewBox="0 0 24 24">
-            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-          </svg>
-        </div>
-        <div class="flex item-center justify-between mt-3">
-          <h1 class="text-gray-700 font-bold text-xl">$220</h1>
-          <button class="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">Add to Card</button>
-        </div>
-      </div>
+<div class="flex flex-col h-screen">
+	<div
+		class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
+		<div class="w-full md:w-1/3 bg-white grid place-items-center">
+			<img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="tailwind logo" class="rounded-xl" />
     </div>
-  </div>
+			<div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+				<div class="flex justify-between item-center">
+					<p class="text-gray-500 font-medium hidden md:block">Vacations</p>
+					<div class="flex items-center">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-500" viewBox="0 0 20 20"
+							fill="currentColor">
+							<path
+								d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+						</svg>
+						<p class="text-gray-600 font-bold text-sm ml-1">
+							4.96
+							<span class="text-gray-500 font-normal">(76 reviews)</span>
+						</p>
+					</div>
+					<div class="">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-pink-500" viewBox="0 0 20 20"
+							fill="currentColor">
+							<path fill-rule="evenodd"
+								d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+								clip-rule="evenodd" />
+						</svg>
+					</div>
+					<div class="bg-gray-200 px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block">
+						Superhost</div>
+				</div>
+				<h3 class="font-black text-gray-800 md:text-3xl text-xl">The Majestic and Wonderful Bahamas</h3>
+				<p class="md:text-lg text-gray-500 text-base">The best kept secret of The Bahamas is the country’s sheer
+					size and diversity. With 16 major islands, The Bahamas is an unmatched destination</p>
+				<p class="text-xl font-black text-gray-800">
+					$110
+					<span class="font-normal text-gray-600 text-base">/night</span>
+				</p>
+			</div>
+		</div>
+	</div>
